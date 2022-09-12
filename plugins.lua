df329b275fa2737eeb870fd2155ef60042be03ec
Daniel Stuessy <danielstuessy@gmail.com>
2022-09-12 16:18:09 +0100

    adding emmet, and configuring lsp plugin

--- plugins.lua
+++ plugins.lua
@@ -1,7 +1,20 @@
 local M = {
   ["BurntSushi/ripgrep"] = {},
   ["tpope/vim-surround"] = {},
-  ["editorconfig/editorconfig-vim"] = {}
+  ["editorconfig/editorconfig-vim"] = {},
+  ["mattn/emmet-vim"] = {},
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  -- ["jose-elias-alvarez/null-ls.nvim"] = {
+  --     after = "nvim-lspconfig",
+  --     config = function()
+  --         require "custom.plugins.null-ls"
+  --     end,
+  -- }
 }
 
 return M
