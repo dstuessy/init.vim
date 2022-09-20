67fb4b3c36af016ddcd0498af021cd49f3618955
Daniel Stuessy <danielstuessy@gmail.com>
2022-09-20 20:53:33 +0100

    plugins: adding denols

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss"}
+local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss", "denols" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
