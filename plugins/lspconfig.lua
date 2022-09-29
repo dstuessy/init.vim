bc09a410c6d403e146bcd21829abbf83ed5258c7
Daniel Stuessy <danielstuessy@gmail.com>
2022-09-29 10:17:26 +0100

    don't autostart denols

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -2,11 +2,12 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss", "denols" }
+local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss", "denols", "phpactor" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
+    autostart = lsp ~= "denols"
   }
 end
