778263611861b5e1221618ba58809593d476f201
Daniel Stuessy <danielstuessy@moneyalive.lan>
2022-11-02 15:20:26 +0000

    mappings: fix buffer change mappings to work with newer nvchad version

--- mappings.lua
+++ mappings.lua
@@ -10,14 +10,14 @@ M.tabufline = {
   n = {
     ["<S-L>"] = {
       function()
-        require("core.utils").tabuflineNext()
+        require("nvchad_ui.tabufline").tabuflineNext()
       end,
       "goto next buffer",
     },
 
     ["<S-H>"] = {
       function()
-        require("core.utils").tabuflinePrev()
+        require("nvchad_ui.tabufline").tabuflinePrev()
       end,
       "goto prev buffer",
     },
