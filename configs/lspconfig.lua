5cb5779023c869338397c53793be6bd966657afe
Daniel Stuessy <danielstuessy@gmail.com>
2024-02-11 20:29:14 +0000

    adding go support

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,7 +3,8 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss", "denols", "phpactor", "rust_analyzer", "arduino_language_server", "csharp_ls" }
+local servers = { "html", "cssls", "clangd", "tsserver", "volar", "tailwindcss", "denols", "phpactor", "rust_analyzer",
+  "arduino_language_server", "csharp_ls", "gopls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
