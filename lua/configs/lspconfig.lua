8850f68253599c0b9bc8a9c508dfcec342b12b69
Daniel Stuessy <daniel.stuessy@mortgagebrain.co.uk>
2025-01-22 10:26:29 +0000

    !feat: upgrading to nvchad 2.5

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,30 @@
-local configs = require "plugins.configs.lspconfig"
+-- load defaults i.e lua_lsp
+-- require("nvchad.configs.lspconfig").defaults()
+--
+-- local lspconfig = require "lspconfig"
+--
+-- -- EXAMPLE
+-- local servers = { "html", "cssls" }
+-- local nvlsp = require "nvchad.configs.lspconfig"
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
+
+
+local configs = require "nvchad.configs.lspconfig"
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
@@ -9,7 +35,6 @@ local servers = {
   "clangd",
   "ts_ls",
   "volar",
-  "tailwindcss",
   "denols",
   "phpactor",
   "rust_analyzer",
---
+++ lua/mappings.lua
@@ -0,0 +1,27 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("n", "<S-b>", "")
+map("n", "<S-L>",
+  function()
+    require("nvchad.tabufline.init").next()
+  end,
+  {
+    desc = "goto next buffer",
+  }
+)
+map("n", "<S-H>",
+  function()
+    require("nvchad.tabufline.init").prev()
+  end,
+  {
+    desc = "goto prev buffer",
+  }
+)
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
