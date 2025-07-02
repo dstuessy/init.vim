8cdc53110d2cd7a01c424b9ddc248d48821d9654
Daniel Stuessy <daniel.stuessy@mortgagebrain.co.uk>
2025-07-02 10:31:40 +0100

    replacing copilot and avante with supermaven

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,101 +38,17 @@ return {
       })
     end,
   },
-  {"zbirenbaum/copilot-cmp",
-    required = { "zbirenbaum/copilot.lua" },
-    enabled = false,
-    config = function ()
-      print("copilot-cmp config")
-      require("copilot_cmp").setup()
-    end
-  },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
+    "supermaven-inc/supermaven-nvim",
     lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    behaviour = {
-        --- ... existing behaviours
-        enable_cursor_planning_mode = true, -- enable cursor planning mode!
-    },
-    opts = {
-      -- add any opts here
-      -- for example
-      -- provider = "copilot",
-      provider = "gemini",
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          -- model = "claude-3-5-haiku-20241022", -- your desired model (or use gpt-4o, etc.)
-          model = "claude-4-sonnet-20250514", -- your desired model (or use gpt-4o, etc.)
-          timeout = 30000, -- timeout in milliseconds
-          extra_request_body = {
-            temperature = 0, -- adjust if needed
-            max_tokens = 4096,
-          }
-          -- reasoning_effort = "high" -- only supported for "o" models
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<A-CR>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
         },
-        gemini = {
-          endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-          model = "gemini-2.5-flash", -- your desired model (or use gpt-4o, etc.)
-          timeout = 30000, -- timeout in milliseconds
-          temperature = 0, -- adjust if needed
-          extra_request_body = {
-              max_tokens = 20480,
-            }
-        },
-        ollama = {
-          endpoint = "http://localhost:11434",
-          model = "qwen2.5-coder:1.5b",
-          extra_request_body = {
-              temperature = 0,
-              max_tokens = 4096,
-            }
-        },
-      },
-      web_search_engine = {
-        provider = "tavily", -- tavily, serpapi, searchapi, google or kagi
-      }
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
+      })
+    end,
   }
 }
